6b8cb41782ca89713fc417dcde948d56c0bea9c1
Idriss Mouhammad Outdili <idriss.outdili@gmail.com>
2024-10-08 03:46:31 +0200

    V3

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -9,4 +9,21 @@
 /*   Updated: 2024/10/08 03:44:25 by moutdili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include	"libft.h"
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t i;
+	char *r;
+	
+	i = 0;
+	r = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!r)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		r[i] = f(i, s[i]);
+		i++;
+	}
+	r[i] = '\0';
+	return (r);
+}
