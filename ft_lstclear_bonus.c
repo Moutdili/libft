f5be0d271408fbf038c566e55644e97d9a5013f1
Idriss Mouhammad Outdili <idriss.outdili@gmail.com>
2024-10-08 11:20:55 +0200

    ft_lstclear_bonus.c

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -9,5 +9,21 @@
 /*   Updated: 2024/10/08 09:38:26 by moutdili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+    t_list *temp;
+
+    if (!lst || !del)
+        return;
+    
+    while (*lst)
+    {
+        temp = (*lst)->next;
+        ft_lstdelone(*lst, del);
+        *lst = temp;
+    }
+    *lst = NULL;
+}
 
