57003617688026e248001ef790ea98eb75ed803d
Idriss Mouhammad Outdili <idriss.outdili@gmail.com>
2024-10-08 11:25:25 +0200

    ft_lstmap_bonus.c

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -9,5 +9,26 @@
 /*   Updated: 2024/10/08 09:38:03 by moutdili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *mapped_list = NULL;
+    t_list *current_node;
 
+    if (!lst || !f)
+        return (NULL);
+    
+    while (lst)
+    {
+        current_node = ft_lstnew(f(lst->content));
+        if (!current_node)
+        {
+            ft_lstclear(&mapped_list, del);
+            return (NULL);
+        }
+        ft_lstadd_back(&mapped_list, current_node);
+        lst = lst->next;
+    }
+    return (mapped_list);
+}
